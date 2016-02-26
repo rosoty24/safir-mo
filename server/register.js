bf55c33d2e12d6314e049aba91e07057f912a077
ZAKRIYA <ZAKRIYA>
2016-02-26 14:58:04 +0700

    work on confirm password strong password and alert message login

--- server/register.js
+++ server/register.js
@@ -1,9 +1,10 @@
 Meteor.methods({
- regUser:function(firstname, lastname, email, password, shipcard, point, rerole,country,city,username){
+ regUser:function(firstname, lastname, email, password,con_password, shipcard, point, rerole,country,city,username){
    targetUserId = Accounts.createUser({
    	username:username,
     email: email,
     password: password,
+    con_password:con_password,
     profile:{firstname:firstname,lastname:lastname,country:country,city:city,shipcard:{shipcardId:shipcard,point:point}}
    });
    console.log(targetUserId);
