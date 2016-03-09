0d5c11090e6062cfc9bfb6a7f79106a9a1c560b1
sokhy <sokhyyin@gmail.com>
2016-03-09 10:40:22 +0700

    related product details

--- client/controllers/recommendation.js
+++ client/controllers/recommendation.js
@@ -19,17 +19,5 @@ Template.recommendation.helpers({
             
         }
         return resultRandom;
-    },
-    getImage: function(id){
-
-            var img = images.findOne({_id:id});
-            console.log(img);
-            
-            if(img){
-                console.log(img.copies.images.key);
-                return img.copies.images.key;
-            }else{
-                return;
-            }
     }
 });
