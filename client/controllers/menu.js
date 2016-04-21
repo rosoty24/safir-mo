eb0bbbf70a1ceffdbcbade3e44c900c0161f10d0
sokhy <sokhyyin@gmail.com>
2016-04-21 16:48:18 +0700

    delete code on product.js

--- client/controllers/menu.js
+++ client/controllers/menu.js
@@ -3,16 +3,12 @@ Session.set('children2','');
 Session.set('selected_menu','');
 Template.mainLayoutMobile.events({
 	'click #en':function(e,tpl){
-		//alert(TAPi18n.getLanguage());
 		e.preventDefault();
 		if(TAPi18n.getLanguage()=='fa'){
 			var lang='en';		
-			//$("body").css("font-family","HelveticaNeue, sans-serif");	
 		}
 		else{	
-
 			var lang='fa';		
-			//$('body').css('font-family','Nazanin Bold');	
 		}
 		Session.set('LANG',lang);
 
@@ -20,7 +16,6 @@ Template.mainLayoutMobile.events({
         Session.set("showLoadingIndicator", false);
       })
       .fail(function (error_message) {
-        // Handle the situation
         console.log(error_message);
       });
 
@@ -45,13 +40,9 @@ Template.menu.events({
 	'click #second_child':function(){
 		$("#second_makeup").slideToggle("hide");
 	},
-	"click #child_child":function(e){
-		//e.preventDefault();
+	"click #child_child":function(){
 		var id = this._id;
 		var name = $(e.currentTarget).text();
-
-		//alert(name);
-		
 		$("#secondChild").html(name);
 		Session.set("FIRSTCHILDID", id);
 		$("#panel_all_makeup").slideToggle("hide");
@@ -64,13 +55,9 @@ Template.menu.events({
         $("#panel_all_makeup").hide();
         $("#second_makeup").hide();
     },
-    "click #child_makeup":function(e){
-    	//e.preventDefault();
-    	//alert("pisey toggle");
-    	//$( "p" ).removeClass( "myClass yourClass" )
+    "click #child_makeup":function(){
     	var parentsId = this._id; 
     	Session.set("PARENTS",parentsId );
-    	//console.log("MY ID CATGORY IS==========="+parentsId);
     	var parentName = $(e.currentTarget).text();
     		$("#makeup").html(parentName);
     		Session.set("parentNAME", parentName);
@@ -82,7 +69,6 @@ Template.menu.events({
     "click #all_makeup":function(e){
     	e.preventDefault();
     	var id = this._id;
-    	//alert(id);
         $("#panel_all_makeup").slideToggle("slow");
         $("#all_makeup").addClass("active");
         $("#panel_makeup").hide();
@@ -98,7 +84,6 @@ Template.menu.helpers({
         });
 	},
 	getChildren: function(){
-		//cnonsole.log("MY_CHILDREN="+id);
 		var id = Session.get("PARENTS");
 		Session.get('LANG');
 		return categories.find({"parent":id}).map(function(document, index) {
@@ -129,6 +114,7 @@ Template.menu.helpers({
 	}
 	
 });
+
 Template.headermenu.onRendered(function () {
 //default font
 $('body').css('font-family','Nazanin Bold');
@@ -141,81 +127,31 @@ $('body').css('font-family','Nazanin Bold');
          			Session.setPersistent('userId',newId);
          			console.log('Newid'+newId);
   }
-  // console.log('MY CART USER ID='+Session.get('userId'));
-  //alert(currentPage);
-	//Meteor.call('getMonIp',userId,time,currenturl);
-	//alert('inserted');
 });
+
 Template.footer.events({
 	'mouseenter #footer':function(e){
 		e.preventDefault();
-		//alert("hi");
 		var userId = Meteor.userId();
 		var time = Date.now();
 		var currenturl = window.location.href
 		var location = 'Footer';
-		//alert(currenturl);
-		//Meteor.call('getfooter',userId,time,location, currenturl);
-		//alert('footer');
 	}
 });
+
 Template.headermenu.events({
 	'mouseenter #header':function(e){
 		e.preventDefault();
-		//alert("hi");
 		var userId = Meteor.userId();
 		var time = Date.now();
 		var currenturl = window.location.href
 		var location = 'Header';
-		//alert(currenturl);
-		//Meteor.call('getheader',userId,time,location,currenturl);
-		//alert('header');
 	}
 });
-// Template.mainLayout.events({
-// 	'mouseenter #mainContent':function(e){
-// 		e.preventDefault();
-// 		var userId = Meteor.userId();
-// 		var time = Date.now();
-// 		var currenturl = window.location.href
-// 		var location = 'Content';
-// 		//alert(currenturl);
-// 		//Meteor.call('contenttrack',userId,time,location,currenturl);
-// 		//alert('contenttrack');
-// 	}
-// });
-
 
 Template.headermenu.events({
-	// 'click #en':function(e,tpl){
-	// 	//alert(TAPi18n.getLanguage());
-	// 	if(TAPi18n.getLanguage()=='fa'){
-	// 		var lang='en';			
-	// 		$("body").css("font-family","HelveticaNeue, sans-serif");
-			
-	// 	}
-	// 	else{	
-
-	// 		var lang='fa';		
-	// 		$('body').css('font-family','Nazanin Bold');
-			
-	// 	}
-			
-	// 	Session.set('LANG',lang);
-		
-		
- //      TAPi18n.setLanguage(lang).done(function () {
- //        Session.set("showLoadingIndicator", false);
- //      })
- //      .fail(function (error_message) {
- //        // Handle the situation
- //        console.log(error_message);
- //      });
-
-	// },
 	'click .kesearch': function(e,tpl){
 		var search=tpl.$("#textToSearch").val();
-		//alert("bb"+search);
 		if(search ==''){
             console.log("Please fill in search box!");
             var currenturl = window.location.href
@@ -226,16 +162,7 @@ Template.headermenu.events({
 			Router.go(url);
 
 		}
-		//var listProducts=products.find({"title":{"$regex": search}});
-		 // if (Router.current().route.getName() == "search") { // new
-   //      products.update(t.data._id, {$set: search});
-   //      Router.go('/properties/' + t.data._id);
-   //  } else { // edit 
-   //      var id = Properties.insert(searchproduct);
-   //      Router.go('/properties/' + id + '/edit');
-   //  }   
-// }
-		
+	
 	}
 
 });
